499536a20595c5dabcd99bc0c0237e644139effc
沈浪熊猫儿 <shenda@ustc.edu>
2023-06-06 22:30:20 +0800

    show xmake version hourly

---
+++ python/dags/mogan.py
@@ -0,0 +1,17 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+
+dag_args = {
+    'owner': 'da',
+    'depends_on_past': False,
+    'start_date': datetime(2023, 6, 1, 0, 0, 0),
+}
+
+with DAG(dag_id='mogan_build_and_test', schedule="@hourly", default_args=dag_args) as dag:
+    task = BashOperator(
+        task_id='show_xmake_version',
+        bash_command='xmake --version | head -n 1',
+    )
+    
+    task
